0f4ebdf23fca63e83d3c9082eed80553614781d0
zach-23 <zyh256331@gmail.com>
2024-08-06 14:32:59 -0400

    turbidity v2

--- AquaSmart_Programs/turbidity.py
+++ AquaSmart_Programs/turbidity.py
@@ -1,21 +1,44 @@
+# import smbus
+# import time
+
+
+# bus = smbus.SMBus(1)
+
+
+# address = 0x48
+
+
+# def read_analog(channel):
+#     if channel < 0 or channel > 3:
+#         return -1
+#     bus.write_byte(address, channel)
+#     value = bus.read_byte(address)
+#     return value
+
+# while True:
+#     turbidity_value = read_analog(0)
+#     print(f"Turbidity Analog Value: {turbidity_value}")
+#     time.sleep(1)
+
+
 import smbus
 import time
 
-
+# Initialize I2C bus
 bus = smbus.SMBus(1)
 
+# PCF8591 address
+pcf8591_address = 0x48  # Ensure this is the correct address
 
-address = 0x48
-
-
-def read_analog(channel):
+def read_pcf8591_analog(channel):
     if channel < 0 or channel > 3:
         return -1
-    bus.write_byte(address, channel)
-    value = bus.read_byte(address)
+    bus.write_byte(pcf8591_address, channel)
+    value = bus.read_byte(pcf8591_address)
     return value
 
 while True:
-    turbidity_value = read_analog(0)
-    print(f"Turbidity Analog Value: {turbidity_value}")
+    # Read value from AIN3
+    value = read_pcf8591_analog(3)  # Change channel to 3
+    print(f"Analog Value from AIN3: {value}")
     time.sleep(1)
